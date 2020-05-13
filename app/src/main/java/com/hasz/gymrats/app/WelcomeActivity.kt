5e88a5dbe56adf2363a089821f477fc0b5537051
Mack Hasz <mack@hasz.email>
2020-05-13 14:04:35 -0700

    wow Im basically and Android dev

--- app/src/main/java/com/hasz/gymrats/app/WelcomeActivity.kt
+++ app/src/main/java/com/hasz/gymrats/app/WelcomeActivity.kt
@@ -2,11 +2,23 @@ package com.hasz.gymrats.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity: AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContentView(R.layout.activity_welcome)
+
+    val getStartedButton = findViewById<Button>(R.id.get_started)
+    val loginButton = findViewById<Button>(R.id.log_in)
+
+    getStartedButton.setOnClickListener {
+      println("Get started")
+    }
+
+    loginButton.setOnClickListener {
+      println("Log in")
+    }
   }
 }
