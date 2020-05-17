398142c2b3bd3e04513d1f68db1b2406a9ac4d3c
Mack Hasz <mack@hasz.email>
2020-05-17 14:31:12 -0700

    copied sunflower gradle

---
+++ app/src/main/java/com/hasz/gymrats/app/GymRatsRootActivity.kt
@@ -0,0 +1,14 @@
+package com.hasz.gymrats.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class GymRatsRootActivity: AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    setContentView(R.layout.activity_gym_rats_root)
+
+  }
+}
