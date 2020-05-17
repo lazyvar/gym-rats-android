8f82278ceb15e29164159c851f76006285db32f5
Mack Hasz <mack@hasz.email>
2020-05-17 14:51:54 -0700

    more welcoming

--- app/src/main/java/com/hasz/gymrats/app/activity/GymRatsRootActivity.kt
+++ app/src/main/java/com/hasz/gymrats/app/activity/GymRatsRootActivity.kt
@@ -1,14 +1,13 @@
-package com.hasz.gymrats.app
+package com.hasz.gymrats.app.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import com.hasz.gymrats.app.R
 
 class GymRatsRootActivity: AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContentView(R.layout.activity_gym_rats_root)
-
   }
 }
