f897081c9de0215a6b84cdfc9cb58dfe470eaf94
Mack Hasz <mack@hasz.email>
2020-05-17 16:23:47 -0700

    nav dat

--- app/src/main/java/com/hasz/gymrats/app/activity/GymRatsRootActivity.kt
+++ app/src/main/java/com/hasz/gymrats/app/activity/GymRatsRootActivity.kt
@@ -3,11 +3,13 @@ package com.hasz.gymrats.app.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hasz.gymrats.app.R
+import androidx.databinding.DataBindingUtil.setContentView
+import com.hasz.gymrats.app.databinding.ActivityGymRatsRootBinding
 
 class GymRatsRootActivity: AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    setContentView(R.layout.activity_gym_rats_root)
+    setContentView<ActivityGymRatsRootBinding>(this, R.layout.activity_gym_rats_root)
   }
 }
