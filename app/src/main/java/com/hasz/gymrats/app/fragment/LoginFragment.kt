4f341e2ea7bf4ae93ce4c23f3616f4f8f91bf622
Mack Hasz <mack@hasz.email>
2020-05-17 19:04:37 -0700

    login layout

--- app/src/main/java/com/hasz/gymrats/app/fragment/LoginFragment.kt
+++ app/src/main/java/com/hasz/gymrats/app/fragment/LoginFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 
 import com.hasz.gymrats.app.R
 import com.hasz.gymrats.app.databinding.FragmentLogInBinding
@@ -18,7 +20,13 @@ class LoginFragment: Fragment() {
     return DataBindingUtil.inflate<FragmentLogInBinding>(
       inflater, R.layout.fragment_log_in, container, false
     ).apply {
+      toolbar.setNavigationOnClickListener {
+        findNavController().popBackStack()
+      }
 
+      loginButton.setOnClickListener {
+
+      }
     }.root
   }
 }
