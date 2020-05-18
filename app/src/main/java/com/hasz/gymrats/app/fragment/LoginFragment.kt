f5d187db476d4d184350c4290ccfc5027fbfd50b
Mack Hasz <mack@hasz.email>
2020-05-17 18:14:31 -0700

    we fragging now

--- app/src/main/java/com/hasz/gymrats/app/fragment/LoginFragment.kt
+++ app/src/main/java/com/hasz/gymrats/app/fragment/LoginFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.hasz.gymrats.app.R
+import com.hasz.gymrats.app.databinding.FragmentLogInBinding
 
 class LoginFragment: Fragment() {
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    return inflater.inflate(R.layout.fragment_log_in, container, false)
+    return DataBindingUtil.inflate<FragmentLogInBinding>(
+      inflater, R.layout.fragment_log_in, container, false
+    ).apply {
+
+    }.root
   }
 }
--- app/src/main/java/com/hasz/gymrats/app/fragment/WelcomeFragment.kt
+++ app/src/main/java/com/hasz/gymrats/app/fragment/WelcomeFragment.kt
@@ -5,27 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.findNavController
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.hasz.gymrats.app.R
+import com.hasz.gymrats.app.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    val view = inflater.inflate(R.layout.fragment_welcome, container, false)
-    val loginButton = view.findViewById<Button>(R.id.log_in)
-
-    loginButton.setOnClickListener {
-      print("WOOOOO")
-
-      val direction = WelcomeFragmentDirections
-        .welcomeToLogin()
-
-      view.findNavController().navigate(direction)
-    }
-
-    return view
+    return DataBindingUtil.inflate<FragmentWelcomeBinding>(
+      inflater, R.layout.fragment_welcome, container, false
+    ).apply {
+      loginButton.setOnClickListener {
+        findNavController().navigate(
+          WelcomeFragmentDirections.welcomeToLogin()
+        )
+      }
+    }.root
   }
 }
