f99f49ce435d1d4144d31b4c4ae14fee1854436c
Mack Hasz <mack@hasz.email>
2020-05-20 14:01:05 -0700

    menu layout

--- app/src/main/java/com/hasz/gymrats/app/activity/MainActivity.kt
+++ app/src/main/java/com/hasz/gymrats/app/activity/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.hasz.gymrats.app.activity
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.hasz.gymrats.app.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContentView(R.layout.activity_main)
+
+    val navController = findNavController(R.id.nav_host)
+    val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+    collapsingToolbarLayout.setupWithNavController(toolbar, navController, appBarConfiguration)
+    setSupportActionBar(toolbar)
+
+    supportActionBar?.apply {
+      setHomeButtonEnabled(true)
+      setDisplayHomeAsUpEnabled(true)
+      setHomeAsUpIndicator(android.R.drawable.ic_menu_always_landscape_portrait)
+    }
+
+    toolbar.setNavigationOnClickListener {
+      drawerLayout.openDrawer(Gravity.START)
+    }
   }
 }
