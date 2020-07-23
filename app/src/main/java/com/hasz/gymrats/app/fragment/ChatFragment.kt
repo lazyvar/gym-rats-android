f3f2250a60505dcd8d1b87a6c084874e678830c6
Mack Hasz <mack@hasz.email>
2020-07-23 07:48:34 -0700

    share photo to chat

--- app/src/main/java/com/hasz/gymrats/app/fragment/ChatFragment.kt
+++ app/src/main/java/com/hasz/gymrats/app/fragment/ChatFragment.kt
@@ -1,26 +1,36 @@
 package com.hasz.gymrats.app.fragment
 
-import android.annotation.SuppressLint
-import android.os.*
+import android.app.ActionBar
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.github.dhaval2404.imagepicker.ImagePicker
 import com.google.android.material.snackbar.Snackbar
 import com.hasz.gymrats.app.R
+import com.hasz.gymrats.app.activity.MainActivity
 import com.hasz.gymrats.app.config.EnvironmentConfig
 import com.hasz.gymrats.app.databinding.FragmentChatBinding
 import com.hasz.gymrats.app.loader.GlideLoader
-
 import com.hasz.gymrats.app.model.Challenge
 import com.hasz.gymrats.app.model.ChatMessage
 import com.hasz.gymrats.app.service.AuthService
+import com.hasz.gymrats.app.service.GService
 import com.hasz.gymrats.app.service.GymRatsApi
 import com.stfalcon.chatkit.messages.MessagesListAdapter
 import kotlinx.android.synthetic.main.fragment_chat.*
 import org.phoenixframework.Channel
 import org.phoenixframework.Socket
+
 
 class ChatFragment: Fragment() {
   private lateinit var challenge: Challenge
@@ -59,6 +69,46 @@ class ChatFragment: Fragment() {
       inflater, R.layout.fragment_chat, container, false
     ).apply {
       messagesList.setAdapter(adapter)
+
+      input.setAttachmentsListener {
+        ImagePicker
+          .with(requireContext() as MainActivity)
+          .compress(1024)
+          .start { resultCode, data ->
+            when (resultCode) {
+              Activity.RESULT_OK -> {
+                val fileUri = data?.data
+
+                AlertDialog.Builder(requireContext()).setMessage("Share photo with group?")
+                  .setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+
+                    GService.uploadImage(fileUri!!) { result ->
+                      result.fold(
+                        onSuccess = { url ->
+                          channel.push("new_msg", payload = mapOf("image_url" to url))
+                        },
+                        onFailure = { error ->
+                          Snackbar.make(requireView(), error.message ?: "Something unpredictable happened.", Snackbar.LENGTH_LONG).show()
+                        }
+                      )
+                    }
+                  }
+                  .setView(ImageView(requireContext()).apply {
+                    setImageURI(fileUri!!)
+                  })
+                  .setCancelable(false)
+                  .setNeutralButton(android.R.string.no, null)
+                  .setIcon(android.R.drawable.ic_dialog_alert)
+                  .create()
+                  .show()
+              }
+              ImagePicker.RESULT_ERROR -> {
+                Snackbar.make(requireView(), ImagePicker.getError(data), Snackbar.LENGTH_LONG).show()
+              }
+            }
+          }
+      }
 
       input.setInputListener { message ->
         val content = message.toString().trim()
