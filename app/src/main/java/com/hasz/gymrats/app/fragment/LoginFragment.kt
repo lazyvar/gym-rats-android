5289a917415616d2fdbf711eafeccdc141b0509e
Mack Hasz <mack@hasz.email>
2020-07-30 14:08:41 -0700

    alert dialog with text thing and then it sends and email to the email

--- app/src/main/java/com/hasz/gymrats/app/fragment/LoginFragment.kt
+++ app/src/main/java/com/hasz/gymrats/app/fragment/LoginFragment.kt
@@ -1,20 +1,25 @@
 package com.hasz.gymrats.app.fragment
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
-
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.hasz.gymrats.app.R
 import com.hasz.gymrats.app.activity.MainActivity
 import com.hasz.gymrats.app.databinding.FragmentLogInBinding
 import com.hasz.gymrats.app.service.AuthService
 import com.hasz.gymrats.app.service.GymRatsApi
+
 
 class LoginFragment: Fragment() {
 
@@ -27,6 +32,32 @@ class LoginFragment: Fragment() {
     ).apply {
       toolbar.setNavigationOnClickListener {
         findNavController().popBackStack()
+      }
+
+      resetPasswordButton.setOnClickListener {
+        val input = TextInputEditText(requireContext())
+        input.hint = "Enter email"
+
+        AlertDialog.Builder(requireContext())
+          .setTitle("Reset password")
+          .setMessage("You will receive an email with next steps.")
+          .setView(input)
+          .setCancelable(false)
+          .setPositiveButton("Send") { _, _ ->
+            GymRatsApi.resetPassword(input.text.toString() ?: "") { result ->
+              result.fold(
+                onSuccess = { _ ->
+                  Snackbar.make(it, "Success! Check your email.", Snackbar.LENGTH_LONG).show()
+                },
+                onFailure = { error ->
+                  Snackbar.make(it, error.message ?: "Something unpredictable happened.", Snackbar.LENGTH_LONG).show()
+                }
+              )
+            }
+          }
+          .setNeutralButton(android.R.string.no, null)
+          .setIcon(android.R.drawable.ic_dialog_alert)
+          .show()
       }
 
       loginButton.setOnClickListener {
@@ -47,6 +78,7 @@ class LoginFragment: Fragment() {
           result.fold(
             onSuccess = { account ->
               AuthService.storeAccount(account = account)
+
               val intent = Intent().apply {
                 context?.let { it -> setClass(it, MainActivity::class.java) }
                 flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
