b242b971275f5520dd86c09f548e83c9487d48ce
Mack Hasz <mack@hasz.email>
2020-11-17 07:06:57 -0800

    branch

--- app/src/main/java/com/hasz/gymrats/app/activity/GymRatsRootActivity.kt
+++ app/src/main/java/com/hasz/gymrats/app/activity/GymRatsRootActivity.kt
@@ -1,9 +1,13 @@
 package com.hasz.gymrats.app.activity
 
+import android.R
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.hasz.gymrats.app.service.AuthService
+import io.branch.referral.Branch
+import io.branch.referral.Branch.BranchReferralInitListener
+
 
 class GymRatsRootActivity: AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,4 +25,26 @@ class GymRatsRootActivity: AppCompatActivity() {
 
     startActivity(intent)
   }
+
+  override fun onStart() {
+    super.onStart()
+
+    Branch.sessionBuilder(this)
+      .withCallback(branchReferralInitListener)
+      .withData(if (intent != null) intent.data else null).init()
+  }
+
+  override fun onNewIntent(intent: Intent?) {
+    super.onNewIntent(intent)
+    setIntent(intent)
+
+    Branch.sessionBuilder(this)
+      .withCallback(branchReferralInitListener).reInit()
+  }
+
+  private val branchReferralInitListener =
+    BranchReferralInitListener { linkProperties, error ->
+      print(linkProperties)
+      print(error)
+    }
 }
